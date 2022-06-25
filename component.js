26f366da0d5b600608601b3964d8070eff53d847
nthiendev <nthiendev@gmail.com>
2022-06-25 20:51:57 +0700

    [UPDATE] my source code

---
+++ component.js
@@ -0,0 +1,7 @@
+import html from "./core.js";
+
+export function App() {
+    return html`
+    <h1>Hello world, I am Thien, I can do anything I want
+    `
+} 
---
+++ script.js
@@ -0,0 +1,6 @@
+import { App } from "./component.js";
+import { attach } from "./store.js";
+
+attach(App, document.querySelector('#root'))
+
+
